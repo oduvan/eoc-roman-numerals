f794ee2f30563a23b8311bc89901c55479bf9700
Alexander Lyabah <a.lyabah@checkio.org>
2019-06-22 12:36:47 +0300

    first commit

---
+++ animation/init.js
@@ -0,0 +1,34 @@
+requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
+    function (extIO, $, ra) {
+        var $tryit;
+        var io = new extIO({
+            functions: {
+                js: 'romanNumerals',
+                python: 'checkio'
+            },
+            tryit:function (this_e) {
+                $tryit = $(this_e.extSetHtmlTryIt(this_e.getTemplate('tryit')));
+
+                $tryit.find('.bn-check').click(function (e) {
+                    var $input = $tryit.find(".input-number");
+                    var data = parseInt($input.val());
+                    if (!data || isNaN(data)) {
+                        $input.val(1);
+                        data = 1;
+                    }
+                    if (data > 3999) {
+                        $input.val(3999);
+                        data = 3999;
+                    }
+                    this_e.extSendToConsoleCheckiO(data)
+                    e.stopPropagation();
+                    return false;
+                });
+            },
+            retConsole: function (ret) {
+                $tryit.find(".checkio-result-in").html(ret);
+            }
+        });
+        io.start();
+    }
+);
---
+++ verification/src/tests.py
@@ -0,0 +1,64 @@
+"""
+TESTS is a dict with all you tests.
+Keys for this will be categories' names.
+Each test is dict with
+    "input" -- input data for user function
+    "answer" -- your right answer
+    "explanation" -- not necessary key, it's using for additional info in animation.
+"""
+
+TESTS = {
+    "Basics": [
+        {"input": 1, "answer": "I"},
+        {"input": 10, "answer": "X"},
+        {"input": 50, "answer": "L"},
+        {"input": 100, "answer": "C"},
+        {"input": 500, "answer": "D"},
+        {"input": 1000, "answer": "M"},
+        {"input": 3999, "answer": "MMMCMXCIX"},
+        {"input": 99, "answer": "XCIX"},
+        {"input": 88, "answer": "LXXXVIII"},
+        {"input": 587, "answer": "DLXXXVII"},
+        {"input": 2387, "answer": "MMCCCLXXXVII"},
+        {"input": 3888, "answer": "MMMDCCCLXXXVIII"},
+        {"input": 76, "answer": "LXXVI"},
+        {"input": 6, "answer": "VI"},
+        {"input": 13, "answer": "XIII"},
+        {"input": 44, "answer": "XLIV"}
+    ],
+    "Extra": [
+        {"input": 164, "answer": "CLXIV"},
+        {"input": 3810, "answer": "MMMDCCCX"},
+        {"input": 1256, "answer": "MCCLVI"},
+        {"input": 473, "answer": "CDLXXIII"},
+        {"input": 817, "answer": "DCCCXVII"},
+        {"input": 3766, "answer": "MMMDCCLXVI"},
+        {"input": 663, "answer": "DCLXIII"},
+        {"input": 2318, "answer": "MMCCCXVIII"},
+        {"input": 2346, "answer": "MMCCCXLVI"},
+        {"input": 602, "answer": "DCII"},
+        {"input": 452, "answer": "CDLII"},
+        {"input": 1605, "answer": "MDCV"},
+        {"input": 3465, "answer": "MMMCDLXV"},
+        {"input": 834, "answer": "DCCCXXXIV"},
+        {"input": 1558, "answer": "MDLVIII"},
+        {"input": 1373, "answer": "MCCCLXXIII"},
+        {"input": 3828, "answer": "MMMDCCCXXVIII"},
+        {"input": 378, "answer": "CCCLXXVIII"},
+        {"input": 3155, "answer": "MMMCLV"},
+        {"input": 2900, "answer": "MMCM"},
+        {"input": 2075, "answer": "MMLXXV"},
+        {"input": 579, "answer": "DLXXIX"},
+        {"input": 3914, "answer": "MMMCMXIV"},
+        {"input": 1403, "answer": "MCDIII"},
+        {"input": 2407, "answer": "MMCDVII"},
+        {"input": 716, "answer": "DCCXVI"},
+        {"input": 1047, "answer": "MXLVII"},
+        {"input": 571, "answer": "DLXXI"},
+        {"input": 1131, "answer": "MCXXXI"},
+        {"input": 777, "answer": "DCCLXXVII"},
+        {"input": 3272, "answer": "MMMCCLXXII"},
+        {"input": 2257, "answer": "MMCCLVII"},
+        {"input": 3072, "answer": "MMMLXXII"}
+    ]
+}
